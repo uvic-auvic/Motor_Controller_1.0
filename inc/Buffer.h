4c4dcc7509f42bb468371059e7a08fd5ac8c3017
lymacasm <lyden.m.smith@gmail.com>
2017-01-23 16:18:50 -0800

    Added comments
    Added comments to Buffer.c and .h

--- inc/Buffer.h
+++ inc/Buffer.h
@@ -9,9 +9,9 @@
 
 typedef struct Buffer{
 	char data[MAX_BUFFER_SIZE][MAX_BUFFER_DATA];
-	unsigned short idx_to_load;
-	unsigned short idx_to_pop;
-	unsigned short overflow_cnt;
+	unsigned short idx_to_load; // Stores index where new element should go
+	unsigned short idx_to_pop; // Stores index of the next element to remove
+	unsigned short overflow_cnt; // Stores the number of buffer overflows
 }Buffer;
 
 //Public functions ------------------------------
--- src/Buffer.c
+++ src/Buffer.c
@@ -1,39 +1,47 @@
-/*Buffer - Linked List implementation*/
+/*Buffer - Circular FIFO Buffer implementation*/
 
 #include "Buffer.h"
 #include <stdlib.h>
 
+// Adds an element to the end of the queue
 extern void Buffer_add(Buffer* b, char* str){
+	// Check if buffer is full
 	if((b->idx_to_load - b->idx_to_pop) >= MAX_BUFFER_SIZE)
 	{
+		// Remove oldest element
 		b->idx_to_pop++;
 		b->overflow_cnt++;
 	}
 
+	// Insert element
 	strcpy(b->data[b->idx_to_load % MAX_BUFFER_SIZE], str);
 	b->idx_to_load++;
 }
 
-// To DeBuffer an integer.
-//Pass in the address to ensure variable scope
+// Removes an element from the front of the queue
 extern void Buffer_pop(Buffer* b, char* data) {
+	// Check if the buffer has anything to pop
 	if(b->idx_to_load > b->idx_to_pop)
 	{
+		// Pop oldest element and store it in data
 		strcpy(data, b->data[b->idx_to_pop % MAX_BUFFER_SIZE]);
 		b->idx_to_pop++;
 	}
 }
 
+// Reset all variables of the buffer
 extern void Buffer_init(Buffer* b){
 	b->idx_to_load = 0;
 	b->idx_to_pop = 0;
 	b->overflow_cnt = 0;
 }
 
+// Get the size of the buffer
 extern int Buffer_size(Buffer* b){
 	return (b->idx_to_load - b->idx_to_pop);
 }
 
+// Get the number of overflows that have occurred
 extern int Buffer_overflow(Buffer* b){
 	return b->overflow_cnt;
 }
